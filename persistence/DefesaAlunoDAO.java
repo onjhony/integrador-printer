81a545bea5a9dc80c9209150b3cd33f0ff0c4e9f
onjhony <onjhony@gmail.com>
2015-05-04 23:56:44 -0300

    Update DefesaAlunoDAO.java

--- persistence/DefesaAlunoDAO.java
+++ persistence/DefesaAlunoDAO.java
@@ -4,4 +4,37 @@ import br.com.alvoradamaringa.domain;
 
 public interface DefesaAlunoDAO extends GenericDAO<Long, DefesaAluno>{
     
+
+        public Defesaaluno() {
+             Defesaaluno defesaaluno1 = new defesaaluno();
+    
+        }
+        public Defesa() {
+            Defesa defesa1 = new defesa();
+        }
+        public Aluno() {
+            Aluno aluno1 = new aluno();
+        }
+        public Pesquisatipotcc() {
+            Pesquisatipotcc pesquisatipotcc1 = new pesquisatipotcc();
+        }
+        public Banca() {
+            Banca banca1 = new banca();
+        }
+        public Defesacomentario() {
+            Defesacomentario defesacomentario1 = defesacomentario();
+        }
+        public nota(){
+            double nota1 = new nota();
+        }   
+        public data{
+         String data1 = new data();
+        }    
+        public status{
+            String status1 = new status();
+        }  
+        public tema{
+            String tema1 = new tema();
+        }    
+    
 }
